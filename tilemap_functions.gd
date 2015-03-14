1874b8e5480321641318b44815235b77400c192e
Kermer <Kermer@users.noreply.github.com>
2015-03-14 18:46:02 +0100

    Update tilemap_functions.gd
    Renamed start_pos to part_pos.

--- tilemap_functions.gd
+++ tilemap_functions.gd
@@ -1,13 +1,13 @@
 extends TileMap
 
 # Add part from other TileMap
-func add_part( pos, part, part_size, start_pos = Vector2(0,0) ):
+func add_part( pos, part, part_size, part_pos = Vector2(0,0) ):
 	if !part extends TileMap:
 		print("Can only create from TileMap!")
 		return
 	pos = world_to_map(pos)
-	for x in range(start_pos.x, start_pos.x + part_size.x):
-		for y in range(start_pos.y, start_pos.y + part_size.y):
+	for x in range(part_pos.x, part_pos.x + part_size.x):
+		for y in range(part_pos.y, part_pos.y + part_size.y):
 			var cell_id = part.get_cell(x, y)
 			if cell_id != -1: # part cell is not empty
 				set_cell(pos.x + x, pos.y + y, cell_id) # apply to our main TileMap
