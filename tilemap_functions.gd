07d333eef7d30def098bca93bfc635be16a598d5
Kermer <Kermer@users.noreply.github.com>
2015-03-14 18:11:14 +0100

    Update tilemap_functions.gd

--- tilemap_functions.gd
+++ tilemap_functions.gd
@@ -6,8 +6,8 @@ func add_part( pos, part, part_size, start_pos = Vector2(0,0) ):
 		print("Can only create from TileMap!")
 		return
 	pos = world_to_map(pos)
-	for x in range(start_pos.x, part_size.x):
-		for y in range(start_pos.y, part_size.y):
+	for x in range(start_pos.x, start_pos.x + part_size.x):
+		for y in range(start_pos.y, start_pos.y + part_size.y):
 			var cell_id = part.get_cell(x, y)
 			if cell_id != -1: # part cell is not empty
 				set_cell(pos.x + x, pos.y + y, cell_id) # apply to our main TileMap
