cc582129179238f2741e42da0906023addba2ed3
Kermer <Kermer@users.noreply.github.com>
2015-04-02 14:42:38 +0200

    get_tilemap_area()

--- tilemap_functions.gd
+++ tilemap_functions.gd
@@ -11,3 +11,13 @@ func add_part( pos, part, part_size, part_pos = Vector2(0,0) ):
 			var cell_id = part.get_cell(x, y)
 			if cell_id != -1: # part cell is not empty
 				set_cell(pos.x + x, pos.y + y, cell_id) # apply to our main TileMap
+
+
+# returns TileMap area (pos + size)
+# credits to: Username ( http://www.godotengine.org/forum/viewtopic.php?f=15&t=1821 )
+func get_tilemap_area(tilemap, world_pos=true):
+	var rect = tilemap.get_item_rect()
+	if world_pos == false:
+		rect.pos = tilemap.world_to_map( rect.pos )
+		rect.size = tilemap.world_to_map( rect.size )
+	return rect
